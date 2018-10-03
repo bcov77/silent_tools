84cca0960db379c0375c8081350ff07e0a975e74
bcov77 <bcov@uw.edu>
2018-10-03 16:30:26 -0700

    Added silent index

--- silent_tools.py
+++ silent_tools.py
@@ -10,73 +10,95 @@
 #sys.path.append(os.path.dirname(distutils.spawn.find_executable("silent_tools.py")))
 #import silent_tools
 
-
-#Quickly parse a silent file without pulling it into memory
-def silent_parse(file, clear_scores=False):
-
-
-    line1 = next(file)
-    line2 = next(file)
-
-    assert( line1.startswith("SEQUENCE:" ) )
-    assert( line2.startswith("SCORE:" ) )
-
-    scoreline = line2.strip()
-    if ( clear_scores ):
-        scoreline = "SCORE: score description"
+import os
+import sys
+import subprocess
+import json
+from collections import defaultdict
 
 
+# Returns the silent index which allows rapid
+#  parsing of the silent file
+def get_silent_index(file):
+
+    index_name = get_index_name(file)
+
+    if ( not os.path.exists( index_name ) ):
+        return build_silent_index(file)
+
+    with open(index_name) as f:
+        silent_index = json.loads(f.read())
+
+    if ( validate_silent_index(file, silent_index) ):
+        return silent_index
+
+    eprint("Warning!! Silent file changed size. Rebuilding index!")
+    return build_silent_index(file)
 
 
-    silent_offsets = {}
-    silent_tags = []
+def cmd(command, wait=True):
+    # print ""
+    # print command
+    the_command = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    if (not wait):
+        return
+    the_stuff = the_command.communicate()
+    return str(the_stuff[0]) + str(the_stuff[1])
 
-    current_tag = ""
-    match_tag = ""
-
-    pos = len(line1) + len(line2)
-    while (True):
-        try:
-            line = next(file)
-        except:
-            break
-
-        line_len = len(line)
-
-        last_pos = pos
-        pos += line_len
-
-        if ( line_len == 0 ):
-            continue
-        if ( line[0] != "S" ):
-            continue
-        if ( line.startswith("SEQUENCE") ):
-            try:
-                next(file)
-            except:
-                pass
-            continue
-        if ( line.startswith("SCORE") ):
-            sp = line.split()
-
-            tag = sp[-1]
-
-            number = 0
-            while ( tag in silent_offsets ):
-                number += 1
-                current_tag = match_tag + "@%i"%number
-
-            silent_offsets[tag] = last_pos
-            silent_tags.append(tag)
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 
-    return silent_offsets, silent_tags, scoreline
+def get_index_name(file):
+    return file + ".idx"
 
 
+def build_silent_index(file):
+    assert(os.path.exists(file))
+
+    with open(file) as f:
+        line1 = next(f)
+        line2 = next(f)
+
+        assert( line1.startswith("SEQUENCE:" ) )
+        assert( line2.startswith("SCORE:" ) )
+
+        scoreline = line2.strip()
+
+    # I'm sorry. If you put description in the name of your pose, well..
+    lines = cmd("command grep --byte-offset SCORE: %s | grep -v description | awk '{print $1,$NF}'"%file).strip().split("\n")
+
+    index = defaultdict(lambda : {}, {})
+    order = []
+
+    for line in lines:
+        # print(line)
+        sp = line.strip().split()
+        index[sp[1]]["seek"] = int(sp[0][:-7])
+        order.append(sp[1])
+
+    size = file_size(file)
+
+    silent_index = {"index":index, "tags":order, "scoreline":scoreline, "size":size}
+
+    try:
+        f = open(file + ".idx", "w")
+        f.write(json.dumps(silent_index))
+        f.close()
+    except:
+        eprint("Warning!!! Unable to save index file. Must reindex every time!")
+
+    return silent_index
+
+def validate_silent_index(file, silent_index):
+    size = file_size(file)
+    return size == silent_index["size"]
+
+def file_size(file):
+    return int(cmd("du -b %s | awk '{print $1}'"%file).strip())
 
 def silent_header(scoreline):
     return "SEQUENCE: A\n%s\n"%scoreline.strip()
-
 
 def silent_body(silent_dict, tags=None):
     if ( tags is None ):
